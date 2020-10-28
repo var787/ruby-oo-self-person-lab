77b6d4dbf96ff5ad8de78a6d6017f55add70d09c
var787 <hegde.rakshan@gmail.com>
2020-10-28 16:33:08 -0500

    commit tree b6b59f0021c8657a26684e80bce2746d1691cdff

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,73 @@
-# your code goes here
+class Person
+    attr_reader :name
+        attr_accessor :bank_account, :hygiene, :happiness
+    
+    def initialize(name)
+    
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness
+        @happiness= 10 if @happiness > 10
+        @happiness= 0  if @happiness < 0
+        @happiness
+    end
+    
+    def hygiene
+        @hygiene= 10 if @hygiene > 10
+        @hygiene= 0  if @hygiene < 0
+        @hygiene
+    end
+    
+    def clean?
+        @hygiene > 7
+    end
+
+    def happy?
+        @happiness > 7
+    end
+
+    def get_paid(salary)
+        self.bank_account += salary
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+    
+    def work_out
+        self.happiness += 2
+        self.hygiene -= 3
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend (friend)
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(convo_person, topic)
+        if topic == 'politics'
+            self.happiness -= 2
+            convo_person.happiness -= 2
+            "blah blah partisan blah lobbyist"
+            elsif topic == 'weather'
+                self.happiness += 1
+            convo_person.happiness += 1
+                "blah blah sun blah rain"
+            else
+                "blah blah blah blah blah"
+        end
+    end
+
+
+end
+
+
+
